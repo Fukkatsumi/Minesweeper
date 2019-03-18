8e4e3d02dc459946ccad2b10165266215cd64bba
Fukkatsumi <fukkatsumi@mail.ua>
2019-03-18 23:15:34 +0200

    Minesweeper ver.1.0.1

--- src/com/minesweeper/Minesweeper.java
+++ src/com/minesweeper/Minesweeper.java
@@ -1,7 +1,21 @@
 package com.minesweeper;
 
+/**
+ * Minesweeper ver.1.0
+ * Console version
+ *
+ * created by Fukkatsumi
+ * last update 18.03.19
+ */
+
 public class Minesweeper {
     public static void main(String... args){
+        start();
+    }
+
+    public static void start(){
+
+        //code..
 
     }
 }
