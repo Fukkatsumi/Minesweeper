1ca741b2a4223813ff039f5870547981fcaab40e
Fukkatsumi <fukkatsumi@mail.ua>
2019-04-29 01:51:39 +0300

    Minesweeper ver.2.0 Menu added

--- src/com/minesweeper/Interface.java
+++ src/com/minesweeper/Interface.java
@@ -6,25 +6,40 @@ import java.awt.*;
 public class Interface {
     private JFrame GUIframe;
 
-    private JButton button(String title, Action action){
+    private JButton button(String title){
         JButton button = new JButton(title);
-        button.setAction(action);
+        button.setActionCommand(title);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setBackground(Color.CYAN.darker());
         return button;
-    }
-
-    private JPanel board(){
-        JPanel boardPanel = new JPanel();
-        return boardPanel;
     }
 
     public JPanel menu(){
         JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.PAGE_AXIS));
+        menuPanel.add(Box.createVerticalGlue());
+        menuPanel.add(button("New Game"));
+        menuPanel.add(Box.createVerticalGlue());
+        menuPanel.add(button("Settings"));
+        menuPanel.add(Box.createVerticalGlue());
+        menuPanel.add(button("Exit"));
+        menuPanel.add(Box.createVerticalGlue());
+        menuPanel.setBackground(Color.darkGray);
         return menuPanel;
+    }
+
+    public void settings(){
+
     }
 
     public JPanel game(){
         JPanel gamePanel = new JPanel();
         return gamePanel;
+    }
+
+    private JPanel board(){
+        JPanel boardPanel = new JPanel();
+        return boardPanel;
     }
 
     public void setView(JPanel panel){
@@ -34,7 +49,7 @@ public class Interface {
 
     public Interface() {
         GUIframe = new JFrame("Minesweeper ver.2.0");
-        GUIframe.setSize(new Dimension(200,300));
+        GUIframe.setSize(new Dimension(220,180));
         GUIframe.setLocationRelativeTo(null);
         GUIframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
