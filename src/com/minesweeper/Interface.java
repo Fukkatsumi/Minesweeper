307c88f759986497409d3c701a7a3f86c4cf3194
Fukkatsumi <fukkatsumi@mail.ua>
2019-05-06 00:54:12 +0300

    Minesweeper ver.2.0 MenuBar added, Game frame updated

--- src/com/minesweeper/Interface.java
+++ src/com/minesweeper/Interface.java
@@ -3,6 +3,7 @@ package com.minesweeper;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,8 +11,96 @@ public class Interface {
     private JFrame GUIframe;
     private JLabel lBombs;
     private JLabel lTimer;
+    private JMenuBar mbGame;
 
     private Map<Point, JButton> board;
+
+
+    public JPanel menu(){
+        JPanel pMenu = new JPanel();
+        pMenu.setLayout(new BoxLayout(pMenu, BoxLayout.PAGE_AXIS));
+        pMenu.add(Box.createVerticalGlue());
+        pMenu.add(button("New Game"));
+        pMenu.add(Box.createVerticalGlue());
+        pMenu.add(button("Settings"));
+        pMenu.add(Box.createVerticalGlue());
+        pMenu.add(button("Exit"));
+        pMenu.add(Box.createVerticalGlue());
+
+        pMenu.setBackground(Color.darkGray);
+        pMenu.setSize(new Dimension(220,180));
+
+        mbGame = null;
+
+        return pMenu;
+    }
+
+    public JPanel settings(){
+        JPanel pSettings = new JPanel();
+        pSettings.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        JLabel lBoardSize = new JLabel("Board size: ");
+        lBoardSize.setForeground(Color.lightGray);
+
+        JLabel lBombCount = new JLabel("Bombs count: ");
+        lBombCount.setForeground(Color.lightGray);
+
+        JTextField boardSizeTF = new JTextField(3);
+        boardSizeTF.setText(Integer.toString(Constants.boardSize));
+        boardSizeTF.setBackground(Color.gray.brighter());
+
+        JTextField bombCountTF = new JTextField(3);
+        bombCountTF.setText(Integer.toString(Constants.bombCount));
+        bombCountTF.setBackground(Color.gray.brighter());
+
+        pSettings.add(lBoardSize, setConstraints(c,0,0));
+        pSettings.add(lBombCount, setConstraints(c,0,1));
+        pSettings.add(boardSizeTF, setConstraints(c,1,0));
+        pSettings.add(bombCountTF, setConstraints(c,1,1));
+        pSettings.add(button("OK"), setConstraints(c,0,2));
+        pSettings.add(button("Cancel"), setConstraints(c,1,2));
+
+        pSettings.setBackground(Color.darkGray);
+        pSettings.setSize(new Dimension(220,180));
+
+        mbGame = null;
+
+        return pSettings;
+    }
+
+    public JPanel game(){
+        JPanel pGame = new JPanel();
+        pGame.setLayout(new BoxLayout(pGame, BoxLayout.PAGE_AXIS));
+        pGame.add(tools());
+        pGame.add(board());
+        pGame.setSize(new Dimension(20 + 18*Constants.boardSize, 55 + 18*Constants.boardSize));
+        setMenuBar();
+        return pGame;
+    }
+
+    private GridBagConstraints setConstraints(GridBagConstraints c, int x, int y){
+        c.gridx = x;
+        c.gridy = y;
+        c.weighty = 2.0;
+        return c;
+    }
+
+    private void setMenuBar(){
+        mbGame = new JMenuBar();
+        JMenu menu = new JMenu("Game");
+        JMenuItem menuItem = new JMenuItem("Quit to menu", KeyEvent.VK_Q);
+        menuItem.setActionCommand("Quit to menu");
+        menu.add(menuItem);
+        menuItem = new JMenuItem("New Game", KeyEvent.VK_N);
+        menuItem.setActionCommand("New Game");
+        menu.add(menuItem);
+        menu.addSeparator();
+        menuItem = new JMenuItem("Exit", KeyEvent.VK_E);
+        menuItem.setActionCommand("Exit");
+        menu.add(menuItem);
+        mbGame.add(menu);
+    }
 
     private JButton button(String title){
         JButton button = new JButton(title);
@@ -21,70 +110,12 @@ public class Interface {
         return button;
     }
 
-    public JPanel menu(){
-        JPanel menuPanel = new JPanel();
-        menuPanel.setLayout(new BoxLayout(menuPanel, BoxLayout.PAGE_AXIS));
-        menuPanel.add(Box.createVerticalGlue());
-        menuPanel.add(button("New Game"));
-        menuPanel.add(Box.createVerticalGlue());
-        menuPanel.add(button("Settings"));
-        menuPanel.add(Box.createVerticalGlue());
-        menuPanel.add(button("Exit"));
-        menuPanel.add(Box.createVerticalGlue());
-        menuPanel.setBackground(Color.darkGray);
-        menuPanel.setSize(new Dimension(220,180));
-        return menuPanel;
-    }
-
-    public JPanel settings(){
-        JPanel settingsPanel = new JPanel();
-        settingsPanel.setLayout(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-
-        JLabel lBoardSize = new JLabel("Board size: ");
-        lBoardSize.setForeground(Color.lightGray);
-
-        settingsPanel.add(lBoardSize, set(c,0,0));
-
-        JLabel lBombCount = new JLabel("Bombs count: ");
-        lBombCount.setForeground(Color.lightGray);
-
-        settingsPanel.add(lBombCount,set(c,0,1));
-
-        JTextField boardSizeTF = new JTextField(3);
-        boardSizeTF.setText(Integer.toString(Constants.boardSize));
-        boardSizeTF.setBackground(Color.gray.brighter());
-
-        settingsPanel.add(boardSizeTF, set(c,1,0));
-
-        JTextField bombCountTF = new JTextField(3);
-        bombCountTF.setText(Integer.toString(Constants.bombCount));
-        bombCountTF.setBackground(Color.gray.brighter());
-
-        settingsPanel.add(bombCountTF, set(c,1,1));
-
-        settingsPanel.add(button("OK"), set(c,0,2));
-        settingsPanel.add(button("Cancel"), set(c,1,2));
-
-        settingsPanel.setBackground(Color.darkGray);
-        settingsPanel.setSize(new Dimension(220,180));
-        return settingsPanel;
-    }
-
-    public JPanel game(){
-        JPanel gamePanel = new JPanel();
-        gamePanel.setLayout(new BoxLayout(gamePanel, BoxLayout.PAGE_AXIS));
-        gamePanel.add(tools());
-        gamePanel.add(board());
-        gamePanel.setSize(new Dimension(20 + 18*Constants.boardSize, 55 + 18*Constants.boardSize));
-        return gamePanel;
-    }
-
     private JPanel tools(){
-        JPanel toolsPanel = new JPanel();
-        toolsPanel.setLayout(new BoxLayout(toolsPanel, BoxLayout.LINE_AXIS));
+        JPanel pTools = new JPanel();
+        pTools.setLayout(new BoxLayout(pTools, BoxLayout.LINE_AXIS));
 
         lBombs = new JLabel();
+        lBombs.setForeground(Color.WHITE);
         refreshBombsCount();
 
         JButton button = new JButton(" ");
@@ -94,33 +125,36 @@ public class Interface {
         button.setBackground(Color.gray);
 
         lTimer = new JLabel("00:00");
+        lTimer.setForeground(Color.WHITE);
 
-        toolsPanel.add(Box.createHorizontalGlue());
-        toolsPanel.add(lBombs);
-        toolsPanel.add(Box.createHorizontalGlue());
-        toolsPanel.add(button);
-        toolsPanel.add(Box.createHorizontalGlue());
-        toolsPanel.add(lTimer);
-        toolsPanel.add(Box.createHorizontalGlue());
-        toolsPanel.setBorder(new EmptyBorder(3,1,1,1));
-        return toolsPanel;
-    }
-
-    public void refreshBombsCount(){
-        lBombs.setText(Integer.toString(Constants.bombCount - Constants.openedBomb));
+        pTools.add(Box.createHorizontalGlue());
+        pTools.add(lBombs);
+        pTools.add(Box.createHorizontalGlue());
+        pTools.add(button);
+        pTools.add(Box.createHorizontalGlue());
+        pTools.add(lTimer);
+        pTools.add(Box.createHorizontalGlue());
+        pTools.setBorder(new EmptyBorder(3,1,1,1));
+        pTools.setBackground(Color.darkGray);
+        return pTools;
     }
 
     private JPanel board(){
-        JPanel boardPanel = new JPanel();
-        boardPanel.setLayout(new GridLayout(Constants.boardSize, Constants.boardSize));
+        JPanel pBoard = new JPanel();
+        pBoard.setLayout(new GridLayout(Constants.boardSize, Constants.boardSize));
         try {
             for (JButton button : board.values()) {
-                boardPanel.add(button);
+                pBoard.add(button);
             }
         }catch (NullPointerException e){
             e.printStackTrace();
         }
-        return boardPanel;
+        pBoard.setBackground(Color.darkGray);
+        return pBoard;
+    }
+
+    public void refreshBombsCount(){
+        lBombs.setText(Integer.toString(Constants.bombCount - Constants.openedBomb));
     }
 
     public void setBoard(Map<Point, Field> board){
@@ -133,16 +167,10 @@ public class Interface {
     }
 
     public void setView(JPanel panel){
+        GUIframe.setJMenuBar(mbGame);
         GUIframe.setContentPane(panel);
         GUIframe.setSize(panel.getSize());
         GUIframe.setVisible(true);
-    }
-
-    private GridBagConstraints set(GridBagConstraints c, int x, int y){
-        c.gridx = x;
-        c.gridy = y;
-        c.weighty = 2.0;
-        return c;
     }
 
     public Interface() {
